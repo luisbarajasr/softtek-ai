1d4b8ce9feb0311b20cd714b8a39b8a033dba136
luisbarajasr <luis.br02@hotmail.com>
2023-09-23 23:26:08 -0600

    ajustes

--- softtek-ai/src/App.js
+++ softtek-ai/src/App.js
@@ -1,45 +1,13 @@
 import React from 'react';
-import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation} from "@ap.cx/react-fullpage";
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from "@ap.cx/react-fullpage";
 import LandingPage from "./components/LandingPage";
 import LandingPageCategory from "./components/LandingPageCategory";
 import backgroundVideo from "./assets/landing_background.mp4";
-
+import Landing_page from './pages/landing.tsx';
 
 function App() {
-  const sectionStyle = "vh-100 w-100"
   return (
-    <Fullpage>
-      <FullpageNavigation />
-        <FullPageSections>
-          <FullpageSection
-            className={sectionStyle}
-            style={{ position: "relative"}}
-          >
-            <video
-              autoPlay
-              muted
-              loop
-              style={{
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                zIndex: "-1"
-              }}
-            >
-              <source src={backgroundVideo} type="video/mp4" />
-            </video>
-            <LandingPage />
-          </FullpageSection>
-
-          <FullpageSection className={sectionStyle + " bg-black "}>
-            <LandingPageCategory />
-          </FullpageSection>
-          <FullpageSection className={sectionStyle}>
-            <h1>Section 3</h1>
-          </FullpageSection>
-        </FullPageSections>
-    </Fullpage>
+    <Landing_page />
   );
 }
 
