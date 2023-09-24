5fc84680f8d085b72325650592eaf60e0cd3f494
luisbarajasr <luis.br02@hotmail.com>
2023-09-24 04:01:50 -0600

    chat component

--- softtek-ai/src/components/LandingPageFirstSlide.js
+++ softtek-ai/src/components/LandingPageFirstSlide.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
 import logoImage from "../assets/logo.png"
 import QuestionTag from "./QuestionTag";
+import Chat from "./chat.jsx"
 
 function LandingPageFirstSlide() {
   const exampleQuestions = [
@@ -13,7 +13,7 @@ function LandingPageFirstSlide() {
     "¿Cuál producto tiene más comentarios?",
     "¿Cuál producto tiene menos comentarios?",
     "¿Qué producto genera más interacción en las redes sociales?"
-    ]
+  ]
 
   function shuffleArray(array) {
     const shuffled = array.slice();
@@ -27,30 +27,24 @@ function LandingPageFirstSlide() {
   const shuffledQuestions = shuffleArray(exampleQuestions);
   const randomQuestions = shuffledQuestions.slice(0, 6);
 
-
   return (
     <div className="d-flex flex-column align-items-center">
       <div className="w-100 p-5 mb-4">
-        <img src={logoImage} alt="logo" style={{width: "100px"}}/>
+        <img src={logoImage} alt="logo" style={{ width: "100px" }} />
       </div>
 
       <div className="d-flex flex-column w-50 align-items-center mb-2">
-        <h1 className="text-white">Softtek x Generative AI</h1>
-        <div className="input-group">
-          <div className="form-floating">
-            <input type="text" className="form-control" id="landing-page-input" placeholder="¿Cuál producto es el más vendido?" />
-            <label htmlFor="landing-page-input">Hazme una pregunta</label>
-          </div>
-          <button className="btn btn-success" type="button">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
+        <div>
+          <h1 className="text-white">Softtek x Generative AI</h1>
         </div>
+
+        <Chat />
       </div>
 
       <div className="w-75 d-flex justify-content-center flex-row flex-wrap row-gap-3 opacity-75">
         {randomQuestions.map((question, index) => {
           return (
-            <QuestionTag key={index} question={question}/>
+            <QuestionTag key={index} question={question} />
           )
         })}
       </div>
